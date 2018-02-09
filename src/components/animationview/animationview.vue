9d74a2a8f904923e779c3e70663dbd30e232b8da
AgoniNemo <XZC168520@163.com>
2018-02-09 18:20:27 +0800

    change animation

--- src/components/animationview/animationview.vue
+++ src/components/animationview/animationview.vue
@@ -1,32 +1,46 @@
 <template>
-  <div class="animation-view">
-    <div class="fsrit">
-      <img class="fsrit-img" src="../../assets/home/back.png" width="100%" height="100%">
+  <transition>
+    <div class="animation-view">
+        <div class="first">
+          <img class="first-img" src="../../assets/home/back.png">
+        </div>
     </div>
-    <div class="second">
-      <img class="second-img" src="../../assets/home/back.png" width="100%" height="100%">
-    </div>
-  </div>
+  </transition>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  export default {
+    data() {
+      return {
+        show: false
+      };
+    },
+    methods: {
+      showInterface() {
+        console.log('sss');
+        this.show = true;
+      }
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
   .animation-view
     display: flex
-    position: absolute
-    background: no-repeat
-    background-size: contain
-    background-color: #464646
-    .fsrit
-      .fsrit-img
-        z-index: 10
-    .second
-      .second-img
-        position: absolute
-        z-index: 10
-        left: 80%
+    .first
+      z-index: 0
+      .first-img
+        position: fixed
+        width: 100%
+        height: 100%
+        animation: firstslidein 6s infinite
+        animation-direction: alternate
+        @keyframes firstslidein
+          0% {
+            opacity: 0.4
+          }
+          100% {
+            opacity: 1
+          }
 
 </style>
