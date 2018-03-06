60e78acac8aa6d687e6e3e23573ed8ed4c43c1e8
AgoniNemo <XZC168520@163.com>
2018-03-06 18:27:47 +0800

    .

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-
+    <div class="nav">
+      <div class="image">
+        <img :src="this.modelModel.headPath">
+      </div>
+    </div>
+    <div class="content"></div>
   </div>
 </template>
 
@@ -28,5 +33,14 @@ import {loadFromLocal} from '@/common/js/store.js';
     bottom: 0;
     left: 0;
     background: #fff;
+    .nav
+      height: 44px;
+      width: 100%;
+      background: #fff
+      box-shadow: 0 10px 18px #888888;
+    .content
+      margin-top: 20px
+      width: 100%
+      height: 50%
 
 </style>
