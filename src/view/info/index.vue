56dcc83639e2610ebf8155dd18ef2fd36ffb0fa3
AgoniNemo <XZC168520@163.com>
2018-08-27 22:17:12 +0800

    添加个人信息头像

--- src/view/info/index.vue
+++ src/view/info/index.vue
@@ -4,21 +4,39 @@
             <div class="info-header">
                 <img  class="info-icon" :src=imageUrl>
             </div>
+            <div>
+                <div class="info-image">
+                    <img :src="iconUrl">
+                </div>
+                <div class="info-name">
+                    {{this.userModel.name}}
+                </div>
+            </div>
         </div>
     </el-card>
 </template>
 
 <script type="text/ecmascript-6">
-    import url from '@/assets/image/nav.jpg';
-    export default {
+import url from '@/assets/image/nav.jpg';
+import icon from '@/assets/image/header.jpg';
+import { mapGetters } from 'vuex';
+
+export default {
 
     computed: {
+        ...mapGetters([
+            'userModel',
+            'videoModel'
+        ]),
         imageUrl() {
             return url;
+        },
+        iconUrl() {
+            return icon;
         }
     }
 
-    };
+};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -30,4 +48,18 @@
         .info-icon
             width: 100%
             border-radius: 2px
+    .info-image
+        display: inline-block
+        position: relative
+        width: 160px
+        height: 160px
+        border: 5px solid #fff
+        background: #fff
+        margin: -20px 0 0 20px;
+        border-radius: 5px;
+    .info-name
+        display: inline-block
+        height: 40px;
+        width: 100px;
+        font-weight: 20px;
 </style>
